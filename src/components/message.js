0df0fa14b7deb86128556c424ccd2709694b564e
Norm- <dorukk@gmail.com>
2016-10-23 16:51:19 +0300

    avatar 16x16

--- src/components/message.js
+++ src/components/message.js
@@ -2,7 +2,7 @@ import React, { PropTypes } from "react";
 
 const ChatMessage = ({ authorId, text, t }, { viewport }) => (
   <li className="messageItem">
-    <img className="avatar" height="100px" width="100px" src={viewport && `http://placehold.it/100x100/${authorId}/000000`} />
+    <img className="avatar" height="16px" width="16px" src={viewport && `http://placehold.it/16x16/${authorId}/000000`} />
     <span className="text">{text}</span>
     <span className="datetime">{t.toDateString()}</span>
   </li>
